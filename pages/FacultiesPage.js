3f7c25330f2d5a9b0458b462fe1002b9c2eb0970
lalit-1729 <lakithakare54726@gmail.com>
2021-04-05 20:32:59 +0530

    Few Faculties Page is added.

---
+++ pages/FacultiesPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Faculties from "../src/Components/Faculties/Faculties";
+import Header from "../src/Components/Header/Header";
+
+function FacultiesPage(){
+    return(
+        <div>
+            <Header/>
+            <Faculties/>
+        </div>
+    );
+};
+
+export default FacultiesPage;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import React from "react";
+import Header from  "../src/Components/Header/Header";
+import Home from "../src/Components/Home/Home";
+import PageLayout from "../src/Components/PageLayout/PageLayout";
 
 function Page(){
     return(
-        <div className="bg-blue-800 p-2 text-gray-8 00">
-                    <div className="m-6">
-                        <p>
-                            Silicon Valley Code Camp is a two day conference in the Silicon Valley focusing on the
-                            Open Source, the latest enterprise focused technologies, so well as other topics Developers 
-                            are interested in the hearing about such as interesting in hearing about.     
-                        </p>
-                    </div>            
-                </div>
+        <div>
+        <Header/>
+        <PageLayout/>
+        {/*<Home/>*/}
+        </div>
     );
 };
 
