b08cca3ad29b48bd458bebce6e173cbfa36b90ca
lalit-1729 <lakithakare54726@gmail.com>
2021-04-05 13:48:17 +0530

    TailwindCss configured and few required folders are created

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Page(){
+    return(
+        <div className="bg-blue-800 p-2 text-gray-8 00">
+                    <div className="m-6">
+                        <p>
+                            Silicon Valley Code Camp is a two day conference in the Silicon Valley focusing on the
+                            Open Source, the latest enterprise focused technologies, so well as other topics Developers 
+                            are interested in the hearing about such as interesting in hearing about.     
+                        </p>
+                    </div>            
+                </div>
+    );
+};
+
+export default Page;
